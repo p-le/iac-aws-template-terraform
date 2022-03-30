2f1036a3b759746d975f0073ca19497399639286
Phu Le <phu.le.dev@gmail.com>
2022-03-30 00:32:38 +0000

    fix workflows, fix add_secret script

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# Set GitHub Actions Secrets to be able to run GitHub Actions Workflow
+# Required Files:
+# -  .env
+# -  infra/terraform.tfvars
+# Please check .example files and create
 
-FILE=.env
-if [ -f "$FILE" ]; then
-    echo "Set GitHub Action Secrets"
+if [ -f ".env" ]; then
+    echo "Set GitHub Action Secrets from .env"
     export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
     gh secret set AWS_ACCESS_KEY_ID --body "$AWS_ACCESS_KEY_ID"
     gh secret set AWS_SECRET_ACCESS_KEY --body "$AWS_SECRET_ACCESS_KEY"
+    gh secret set AWS_REGION --body "$AWS_REGION"
 else
     echo "Please create .env file!"
+fi
+
+if [ -f "infra/terraform.tfvars" ]; then
+    echo "Set GitHub Action Secrets from terraform.tfvars"
+    base64 infra/terraform.tfvars > infra/terraform.tfvars.base64
+    gh secret set TERRAFORM_TFVARS < infra/terraform.tfvars.base64
+    rm -rf infra/terraform.tfvars.base64
+else
+    echo "Please create infra/terraform.tfvars file!"
 fi
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ -d "~/.tfenv" ]; then
-echo "-- Install tfenv"
-git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-sudo ln -s ~/.tfenv/bin/* /usr/local/bin
-echo "-- Please run `tfenv install latest` to install Terraform"
-fi
-
 echo "-- Install GitHub CLI"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
